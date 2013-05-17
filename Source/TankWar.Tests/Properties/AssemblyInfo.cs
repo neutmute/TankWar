54f913718f55eddf414554f95b45abbcfb9384ca
neutmute <neutmute@users.noreply.github.com>
2013-05-17 22:19:34 +1000

    initial setup

--- Source/TankWar.Tests/Properties/AssemblyInfo.cs
+++ Source/TankWar.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TankWar")]
+[assembly: AssemblyTitle("TankWar.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TankWar")]
+[assembly: AssemblyProduct("TankWar.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b6456485-52d1-4b07-90fe-585899500d20")]
+[assembly: Guid("5e7ada81-f061-4b6d-b03e-0e1ebc399048")]
 
 // Version information for an assembly consists of the following four values:
 //
