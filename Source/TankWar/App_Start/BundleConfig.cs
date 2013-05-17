70191bfdc5c428d1b64df003baecf464fadda27d
neutmute <neutmute@users.noreply.github.com>
2013-05-17 22:53:31 +1000

    bootstrapped

--- Source/TankWar/App_Start/BundleConfig.cs
+++ Source/TankWar/App_Start/BundleConfig.cs
@@ -8,6 +8,10 @@ namespace TankWar
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/signalr").Include(
+                        "~/Scripts/jquery.signalR-{version}.js",
+                        "~/signalr/hubs"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
@@ -23,7 +27,10 @@ namespace TankWar
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/bootstrap-responsive"
+                ,"~/Content/bootstrap.css")
+            );
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
