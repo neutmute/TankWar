925ca3db016030f47409162b54a62ee191d5c176
neutmute <neutmute@users.noreply.github.com>
2013-05-21 23:23:01 +1000

    knockout uses halfings, get admin page working better

--- Source/TankWar/Models/ViewPortModels.cs
+++ Source/TankWar/Models/ViewPortModels.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using TankWar.Engine.Dto;
@@ -8,8 +10,12 @@ namespace TankWar.Models
 {
     public class ViewPortAdminModel
     {
+        [DisplayName("GameLoop Interval (ms)")]
+        [Range(0, 10000.00, ErrorMessage = "This could take a while...")]
         public double GameLoopIntervalMilliseconds { get; set; }
 
+        [DisplayName("Countdown (seconds)")]
+        [Range(0, 300.00, ErrorMessage = "You want to wait that long, really?")]
         public int CountdownSeconds { get; set; }
 
         public override string ToString()
